e72f17e8c2487431a29809272dfde4c9fcb456ea
GautamPoddar18 <gautam.34pr@gmail.com>
2022-06-01 13:17:38 +0530

    Adding dummy function to numpy

---
+++ dumpy_feature.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 13:13:15 2022
+
+@author: GAUTAM PODDAR
+"""
+
+def dummy_feature():
+    print('Adding a dummy feature in open source numpy library')
+    
